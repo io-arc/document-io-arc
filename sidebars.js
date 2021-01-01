cf71a99ed8b4ec4dea400e0984e5bd014401f95d
Nobuyuki Kondo <kondo@arc-one.jp>
2021-01-01 10:17:31 +0900

    feat: Adding CSS documents

--- sidebars.js
+++ sidebars.js
@@ -6,7 +6,7 @@ module.exports = {
       'configuration/site',
       'configuration/build'
     ],
-    Build: ['build/html'],
+    Build: ['build/html', 'build/css'],
     Plugins: ['plugins/plugins'],
     Example: ['addTag', 'doc1', 'mdx']
   }
